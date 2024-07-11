58cfc7d45030d28964009e025787a94965adc793
basem909 <basemshams30@gmail.com>
2024-07-11 17:38:38 -0300

    Separate the view into partials

--- app/controllers/statistics_controller.rb
+++ app/controllers/statistics_controller.rb
@@ -2,5 +2,8 @@ class StatisticsController < ApplicationController
   def index
     @most_searched_ceps = CepSearchLog.most_searched_ceps
     @ceps_by_state = CepSearchLog.ceps_by_state
+    @most_searched_ceps_by_state = CepSearchLog.most_searched_ceps_by_state
+
+    Rails.logger.debug @most_searched_ceps_by_state.inspect
   end
 end
--- app/models/cep_search_log.rb
+++ app/models/cep_search_log.rb
@@ -2,15 +2,15 @@
 class CepSearchLog < ApplicationRecord
   validates :cep, :city, :state, presence: true
 
-  def self.most_searched_ceps(limit = 10)
-    where.group(:cep).order('count_id DESC').count(:id).first(limit)
+  def self.most_searched_ceps(limit = 5)
+    group(:cep).order('count_id DESC').count(:id).first(limit)
   end
 
-  def self.ceps_by_state(limit = 10)
-    where.group(:state).order('count_id DESC').count(:id).first(limit)
+  def self.ceps_by_state(limit = 8)
+    group(:state).order('count_id DESC').count(:id).first(limit)
   end
 
-  def self.most_searched_ceps_by_state(limit = 10)
+  def self.most_searched_ceps_by_state(limit = 5)
     select('state, cep, COUNT(id) as count_id')
       .group('state, cep')
       .order('count_id DESC')
